5c2cf7b7b6c5de2b038c11deb8f0f5897e7bd3d7
MillenniumEarl <MillenniumEarl@users.noreply.github.com>
2021-02-14 12:25:47 +0100

    Add latest PHP URL

--- src/scripts/constants/url.ts
+++ src/scripts/constants/url.ts
@@ -4,4 +4,5 @@ export const urls = {
     F95_LATEST_UPDATES: "https://f95zone.to/latest",
     F95_LOGIN_URL: "https://f95zone.to/login/login",
     F95_WATCHED_THREADS: "https://f95zone.to/watched/threads",
+    F95_LATEST_PHP: "https://f95zone.to/new_latest.php",
 };
