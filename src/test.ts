dac5e9f16b7babb372d35fa23c8c20a663e5e889
MillenniumEarl <MillenniumEarl@users.noreply.github.com>
2021-02-21 13:03:32 +0100

    Add login to test

--- src/test.ts
+++ src/test.ts
@@ -2,9 +2,11 @@
 
 import { fetchLatest } from "./scripts/latest-fetch.js";
 import SearchQuery from "./scripts/classes/search-query.js";
+import { login } from "./index.js";
 
 
 async function test() {
+    const result = await login("MillenniumEarl", "f9vTcRNuvxj4YpK");
     const query = new SearchQuery();
     const urls = await fetchLatest(query, 5);
 }
