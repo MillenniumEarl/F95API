7770b9532237d6a6198206804f410bcf36331ef5
MillenniumEarl <MillenniumEarl@users.noreply.github.com>
2021-02-21 14:53:50 +0100

    Add URLs for alerts, conversations and bookmarks

--- src/scripts/constants/url.ts
+++ src/scripts/constants/url.ts
@@ -5,4 +5,7 @@ export const urls = {
     F95_LOGIN_URL: "https://f95zone.to/login/login",
     F95_WATCHED_THREADS: "https://f95zone.to/watched/threads",
     F95_LATEST_PHP: "https://f95zone.to/new_latest.php",
+    F95_BOOKMARKS: "https://f95zone.to/account/bookmarks",
+    F95_CONVERSATIONS: "https://f95zone.to/conversations/",
+    F95_ALERTS: "https://f95zone.to/account/alerts"
 };
