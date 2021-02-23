8abaf0b8cf692975046999324895c882e5fc12f5
MillenniumEarl <MillenniumEarl@users.noreply.github.com>
2021-02-23 19:55:07 +0100

    Update search url

--- src/scripts/constants/url.ts
+++ src/scripts/constants/url.ts
@@ -1,6 +1,6 @@
 export const urls = {
     F95_BASE_URL: "https://f95zone.to",
-    F95_SEARCH_URL: "https://f95zone.to/search/?type=post",
+    F95_SEARCH_URL: "https://f95zone.to/search",
     F95_LATEST_UPDATES: "https://f95zone.to/latest",
     F95_LOGIN_URL: "https://f95zone.to/login/login",
     F95_WATCHED_THREADS: "https://f95zone.to/watched/threads",
