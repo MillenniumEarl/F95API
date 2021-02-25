827564ca05cda947002f452b115ae1155b42a978
MillenniumEarl <MillenniumEarl@users.noreply.github.com>
2021-02-25 19:06:36 +0100

    Add custom errors

---
+++ src/scripts/classes/errors.ts
@@ -0,0 +1,44 @@
+"use strict";
+
+interface IBaseError {
+    /**
+     * Unique identifier of the error.
+     */
+    id: number,
+    /**
+     * Error message.
+     */
+    message: string,
+    /**
+     * Error to report.
+     */
+    error: Error,
+}
+
+export class GenericAxiosError extends Error implements IBaseError {
+    id: number;
+    message: string;
+    error: Error;
+
+    constructor(args: IBaseError) {
+        super();
+        this.id = args.id;
+        this.message = args.message;
+        this.error = args.error;
+    }
+}
+
+export class UnexpectedResponseContentType extends Error implements IBaseError {
+    id: number;
+    message: string;
+    error: Error;
+
+    constructor(args: IBaseError) {
+        super();
+        this.id = args.id;
+        this.message = args.message;
+        this.error = args.error;
+    }
+}
+
+export class InvalidF95Token extends Error {}
