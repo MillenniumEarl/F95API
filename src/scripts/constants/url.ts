e3eefe72f7e80f4138de601cb27f69ecaf94de57
MillenniumEarl <MillenniumEarl@users.noreply.github.com>
2021-03-02 11:49:02 +0100

    Add urls

--- src/scripts/constants/url.ts
+++ src/scripts/constants/url.ts
@@ -6,6 +6,19 @@ export const urls = {
     F95_WATCHED_THREADS: "https://f95zone.to/watched/threads",
     F95_LATEST_PHP: "https://f95zone.to/new_latest.php",
     F95_BOOKMARKS: "https://f95zone.to/account/bookmarks",
+    /**
+     * Add the unique ID of the post to 
+     * get the thread page where the post 
+     * is present.
+     */
+    F95_POSTS: "https://f95zone.to/posts/",
+    /**
+     * @todo
+     */
     F95_CONVERSATIONS: "https://f95zone.to/conversations/",
-    F95_ALERTS: "https://f95zone.to/account/alerts"
+    /**
+     * @todo
+     */
+    F95_ALERTS: "https://f95zone.to/account/alerts",
+    F95_POSTS_NUMBER: "https://f95zone.to/account/dpp-update",
 };
