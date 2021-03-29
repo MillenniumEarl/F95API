b646014c0727323b1fe2922437efe16144f3a88d
MillenniumEarl <MillenniumEarl@users.noreply.github.com>
2021-03-29 10:18:40 +0200

    Reduce complexity of getURLsFromQuery

--- src/scripts/search.ts
+++ src/scripts/search.ts
@@ -12,18 +12,23 @@ import { HandiworkSearchQuery, LatestSearchQuery, ThreadSearchQuery } from "..";
 import fetchHandiworkURLs from "./fetch-data/fetch-handiwork";
 import fetchLatestHandiworkURLs from "./fetch-data/fetch-latest";
 import fetchThreadHandiworkURLs from "./fetch-data/fetch-thread";
+import { UserNotLogged, USER_NOT_LOGGED } from "./classes/errors";
+import shared from "./shared";
 
 /**
  * Gets the handiworks that match the passed parameters.
  *
  * You *must* be logged.
- * @param {Number} limit
- * Maximum number of items to get. Default: 30
+ * @param {IQuery} query Parameters used for the search.
+ * @param {Number} limit Maximum number of items to get. Default: 30
  */
 export default async function search<T extends IBasic>(
   query: IQuery,
   limit: number = 30
 ): Promise<T[]> {
+  // Check if the user is logged
+  if (!shared.isLogged) throw new UserNotLogged(USER_NOT_LOGGED);
+
   // Fetch the URLs
   const urls: string[] = await getURLsFromQuery(query, limit);
 
@@ -36,21 +41,35 @@ export default async function search<T extends IBasic>(
 //#region Private methods
 
 /**
+ * Gets the URLs of the Handiworks that are defined by the search query.
+ *
  * @param query Query used for the search
  * @param limit Maximum number of items to get. Default: 30
  * @returns URLs of the fetched games
  */
 async function getURLsFromQuery(query: IQuery, limit = 30): Promise<string[]> {
-  switch (query.itype) {
-    case "HandiworkSearchQuery":
-      return fetchHandiworkURLs(query as HandiworkSearchQuery, limit);
-    case "LatestSearchQuery":
-      return fetchLatestHandiworkURLs(query as LatestSearchQuery, limit);
-    case "ThreadSearchQuery":
-      return fetchThreadHandiworkURLs(query as ThreadSearchQuery, limit);
-    default:
-      throw Error(`Invalid query type: ${query.itype}`);
+  const functionMap = {
+    HandiworkSearchQuery: (query: IQuery, limit: number) =>
+      fetchHandiworkURLs(query as HandiworkSearchQuery, limit),
+    LatestSearchQuery: (query: IQuery, limit: number) =>
+      fetchLatestHandiworkURLs(query as LatestSearchQuery, limit),
+    ThreadSearchQuery: (query: IQuery, limit: number) =>
+      fetchThreadHandiworkURLs(query as ThreadSearchQuery, limit)
+  };
+
+  const typeMap = {
+    HandiworkSearchQuery: HandiworkSearchQuery,
+    LatestSearchQuery: LatestSearchQuery,
+    ThreadSearchQuery: ThreadSearchQuery
+  };
+
+  // Throws error if the type of query is non existent
+  if (!Object.keys(functionMap).includes(query.itype)) {
+    throw Error(`Invalid query type: ${query.itype}`);
   }
+
+  // fetch and return the urls
+  return await functionMap[query.itype](query, limit);
 }
 
 //#endregion Private methods
