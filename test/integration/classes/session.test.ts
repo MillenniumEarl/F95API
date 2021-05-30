cde8803aa7cc3db7abd5241a3b374a026f4084c9
MillenniumEarl <22562005+MillenniumEarl@users.noreply.github.com>
2021-05-30 17:10:13 +0200

    Remove description

--- test/integration/classes/session.test.ts
+++ test/integration/classes/session.test.ts
@@ -13,87 +13,85 @@ const PASSWORD = "Password";
 const TOKEN = "test-token";
 
 export function suite(): void {
-  describe("Test class Session", () => {
-    it("Session - create", () => {
-      // Arguments
-      const path = "./sessionCreateTest";
+  it("Session - create", () => {
+    // Arguments
+    const path = "./sessionCreateTest";
 
-      // Method call
-      const session = createSession(path);
+    // Method call
+    const session = createSession(path);
 
-      expect(session.token).to.be.equal(TOKEN);
-      expect(session.path).to.be.equal(path);
-      expect(session.isMapped).to.be.false;
-      expect(session.created).to.not.be.null;
-      expect(session.hash).to.not.be.null;
-    });
-
-    it("Session - save", async () => {
-      // Arguments
-      const path = "./sessionSaveTest";
-
-      // Method call
-      const session = createSession(path);
-      await session.save();
-
-      // Verify test
-      const exists = fs.existsSync(path);
-      expect(exists).to.be.true;
-
-      // Delete file
-      if (exists) fs.unlinkSync(path);
-    });
-
-    it("Session - load", async () => {
-      // Arguments
-      const path = "./sessionLoadTest";
-
-      // Method call
-      const sessionSave = createSession(path);
-      await sessionSave.save();
-
-      const sessionLoad = createSession(path);
-      await sessionLoad.load();
-
-      // Verify test
-      expect(sessionSave.hash).to.be.equal(sessionLoad.hash);
-      expect(sessionSave.created.toISOString()).to.be.equal(sessionLoad.created.toISOString());
-      expect(sessionSave.token).to.be.equal(sessionLoad.token);
-
-      // Delete file
-      if (fs.existsSync(path)) fs.unlinkSync(path);
-    });
-
-    it("Session - delete", async () => {
-      // Arguments
-      const path = "./sessionDeleteTest";
-
-      // Method call
-      const session = createSession(path);
-      await session.save();
-      await session.delete();
-
-      // Verify test
-      const exists = fs.existsSync(path);
-      expect(exists).to.be.false;
-    });
-
-    it("Session - isValid", () => {
-      // Arguments
-      const path = "./sessionValidateTest";
-
-      // Method call
-      const session = createSession(path);
-      const result = session.isValid(USERNAME, PASSWORD);
-
-      // Expect result
-      expect(result).to.be.true;
-    });
+    expect(session.token).to.be.equal(TOKEN);
+    expect(session.path).to.be.equal(path);
+    expect(session.isMapped).to.be.false;
+    expect(session.created).to.not.be.null;
+    expect(session.hash).to.not.be.null;
   });
 
-  function createSession(path: string) {
-    const session = new Session(path);
-    session.create(USERNAME, PASSWORD, TOKEN);
-    return session;
-  }
+  it("Session - save", async () => {
+    // Arguments
+    const path = "./sessionSaveTest";
+
+    // Method call
+    const session = createSession(path);
+    await session.save();
+
+    // Verify test
+    const exists = fs.existsSync(path);
+    expect(exists).to.be.true;
+
+    // Delete file
+    if (exists) fs.unlinkSync(path);
+  });
+
+  it("Session - load", async () => {
+    // Arguments
+    const path = "./sessionLoadTest";
+
+    // Method call
+    const sessionSave = createSession(path);
+    await sessionSave.save();
+
+    const sessionLoad = createSession(path);
+    await sessionLoad.load();
+
+    // Verify test
+    expect(sessionSave.hash).to.be.equal(sessionLoad.hash);
+    expect(sessionSave.created.toISOString()).to.be.equal(sessionLoad.created.toISOString());
+    expect(sessionSave.token).to.be.equal(sessionLoad.token);
+
+    // Delete file
+    if (fs.existsSync(path)) fs.unlinkSync(path);
+  });
+
+  it("Session - delete", async () => {
+    // Arguments
+    const path = "./sessionDeleteTest";
+
+    // Method call
+    const session = createSession(path);
+    await session.save();
+    await session.delete();
+
+    // Verify test
+    const exists = fs.existsSync(path);
+    expect(exists).to.be.false;
+  });
+
+  it("Session - isValid", () => {
+    // Arguments
+    const path = "./sessionValidateTest";
+
+    // Method call
+    const session = createSession(path);
+    const result = session.isValid(USERNAME, PASSWORD);
+
+    // Expect result
+    expect(result).to.be.true;
+  });
+}
+
+function createSession(path: string) {
+  const session = new Session(path);
+  session.create(USERNAME, PASSWORD, TOKEN);
+  return session;
 }
--- test/unit/classes/session.test.ts
+++ test/unit/classes/session.test.ts
@@ -9,23 +9,21 @@ import { expect } from "chai";
 import Session from "../../../src/scripts/classes/session";
 
 export function suite(): void {
-  describe("Test class Session", () => {
-    it("Session - null path", () => {
-      // Arguments
-      const path = null;
-      const ERROR_MESSAGE = "Invalid path for the session file";
+  it("Session - null path", () => {
+    // Arguments
+    const path = null;
+    const ERROR_MESSAGE = "Invalid path for the session file";
 
-      // Method call
-      expect(() => new Session(path)).throw(ERROR_MESSAGE);
-    });
+    // Method call
+    expect(() => new Session(path)).throw(ERROR_MESSAGE);
+  });
 
-    it("Session - empty path", () => {
-      // Arguments
-      const path = "";
-      const ERROR_MESSAGE = "Invalid path for the session file";
+  it("Session - empty path", () => {
+    // Arguments
+    const path = "";
+    const ERROR_MESSAGE = "Invalid path for the session file";
 
-      // Method call
-      expect(() => new Session(path)).throw(ERROR_MESSAGE);
-    });
+    // Method call
+    expect(() => new Session(path)).throw(ERROR_MESSAGE);
   });
 }
