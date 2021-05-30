86edd7088f694cbac482d025375f3bedb38b8f1c
MillenniumEarl <22562005+MillenniumEarl@users.noreply.github.com>
2021-05-30 14:45:36 +0200

    Split test for class

--- test/unit/classes/errors.test.ts
+++ test/unit/classes/errors.test.ts
@@ -19,79 +19,85 @@ export function suite(): void {
   const MESSAGE = "Test message";
   const ERROR = new Error("Test error");
 
-  it("BaseAPIError - Null ID", () => {
-    // Arguments
-    const args = { id: null, message: MESSAGE, error: ERROR };
+  describe("Test class BaseAPIError", () => {
+    it("BaseAPIError - Null ID", () => {
+      // Arguments
+      const args = { id: null, message: MESSAGE, error: ERROR };
 
-    // Property call
-    expect(() => new BaseAPIError(args)).throw(INVALID_ERROR_ID);
+      // Property call
+      expect(() => new BaseAPIError(args)).throw(INVALID_ERROR_ID);
+    });
+
+    it("BaseAPIError - Negative ID", () => {
+      // Arguments
+      const args = { id: -1, message: MESSAGE, error: ERROR };
+
+      // Property call
+      expect(() => new BaseAPIError(args)).throw(INVALID_ERROR_ID);
+    });
+
+    it("BaseAPIError - Null message", () => {
+      // Arguments
+      const args = { id: ID, message: null, error: ERROR };
+
+      // Property call
+      expect(() => new BaseAPIError(args)).throw(INVALID_ERROR_MESSAGE);
+    });
+
+    it("BaseAPIError - Empty message", () => {
+      // Arguments
+      const args = { id: ID, message: "", error: ERROR };
+
+      // Property call
+      expect(() => new BaseAPIError(args)).throw(INVALID_ERROR_MESSAGE);
+    });
+
+    it("BaseAPIError - Null error", () => {
+      // Arguments
+      const args = { id: ID, message: MESSAGE, error: null };
+
+      // Property call
+      expect(() => new BaseAPIError(args)).throw(INVALID_ERROR_ERROR);
+    });
+
+    it("BaseAPIError - Valid arguments", () => {
+      // Arguments
+      const args = { id: ID, message: MESSAGE, error: ERROR };
+
+      // Property call
+      const baseAPIError = new BaseAPIError(args);
+
+      expect(baseAPIError.id).to.be.equal(ID, "ID value must be equal");
+      expect(baseAPIError.message).to.be.equal(MESSAGE, "Message value must be equal");
+      expect(baseAPIError.error).to.be.equal(ERROR, "Error value must be equal");
+    });
   });
 
-  it("BaseAPIError - Negative ID", () => {
-    // Arguments
-    const args = { id: -1, message: MESSAGE, error: ERROR };
+  describe("Test class GenericAxiosError", () => {
+    it("GenericAxiosError - Valid arguments", () => {
+      // Arguments
+      const args = { id: ID, message: MESSAGE, error: ERROR };
 
-    // Property call
-    expect(() => new BaseAPIError(args)).throw(INVALID_ERROR_ID);
+      // Property call
+      const genericAxiosError = new GenericAxiosError(args);
+
+      expect(genericAxiosError.id).to.be.equal(ID, "ID value must be equal");
+      expect(genericAxiosError.message).to.be.equal(MESSAGE, "Message value must be equal");
+      expect(genericAxiosError.error).to.be.equal(ERROR, "Error value must be equal");
+    });
   });
 
-  it("BaseAPIError - Null message", () => {
-    // Arguments
-    const args = { id: ID, message: null, error: ERROR };
+  describe("Test class UnexpectedResponseContentType", () => {
+    it("UnexpectedResponseContentType - Valid arguments", () => {
+      // Arguments
+      const args = { id: ID, message: MESSAGE, error: ERROR };
 
-    // Property call
-    expect(() => new BaseAPIError(args)).throw(INVALID_ERROR_MESSAGE);
-  });
+      // Property call
+      const error = new UnexpectedResponseContentType(args);
 
-  it("BaseAPIError - Empty message", () => {
-    // Arguments
-    const args = { id: ID, message: "", error: ERROR };
-
-    // Property call
-    expect(() => new BaseAPIError(args)).throw(INVALID_ERROR_MESSAGE);
-  });
-
-  it("BaseAPIError - Null error", () => {
-    // Arguments
-    const args = { id: ID, message: MESSAGE, error: null };
-
-    // Property call
-    expect(() => new BaseAPIError(args)).throw(INVALID_ERROR_ERROR);
-  });
-
-  it("BaseAPIError - Valid arguments", () => {
-    // Arguments
-    const args = { id: ID, message: MESSAGE, error: ERROR };
-
-    // Property call
-    const baseAPIError = new BaseAPIError(args);
-
-    expect(baseAPIError.id).to.be.equal(ID, "ID value must be equal");
-    expect(baseAPIError.message).to.be.equal(MESSAGE, "Message value must be equal");
-    expect(baseAPIError.error).to.be.equal(ERROR, "Error value must be equal");
-  });
-
-  it("GenericAxiosError - Valid arguments", () => {
-    // Arguments
-    const args = { id: ID, message: MESSAGE, error: ERROR };
-
-    // Property call
-    const genericAxiosError = new GenericAxiosError(args);
-
-    expect(genericAxiosError.id).to.be.equal(ID, "ID value must be equal");
-    expect(genericAxiosError.message).to.be.equal(MESSAGE, "Message value must be equal");
-    expect(genericAxiosError.error).to.be.equal(ERROR, "Error value must be equal");
-  });
-
-  it("UnexpectedResponseContentType - Valid arguments", () => {
-    // Arguments
-    const args = { id: ID, message: MESSAGE, error: ERROR };
-
-    // Property call
-    const unexpectedContentTypeError = new UnexpectedResponseContentType(args);
-
-    expect(unexpectedContentTypeError.id).to.be.equal(ID, "ID value must be equal");
-    expect(unexpectedContentTypeError.message).to.be.equal(MESSAGE, "Message value must be equal");
-    expect(unexpectedContentTypeError.error).to.be.equal(ERROR, "Error value must be equal");
+      expect(error.id).to.be.equal(ID, "ID value must be equal");
+      expect(error.message).to.be.equal(MESSAGE, "Message value must be equal");
+      expect(error.error).to.be.equal(ERROR, "Error value must be equal");
+    });
   });
 }
