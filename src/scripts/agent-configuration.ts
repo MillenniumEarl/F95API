0f8a6d499b3703170c705cfe25bd1a588eb104cd
MillenniumEarl <22562005+MillenniumEarl@users.noreply.github.com>
2021-06-03 16:45:40 +0200

    Remove local forage

--- src/scripts/agent-configuration.ts
+++ src/scripts/agent-configuration.ts
@@ -7,34 +7,19 @@
 
 // Public imports from npm
 import axiosCookieJarSupport from "axios-cookiejar-support";
-import localforage from "localforage";
-import memoryDriver from "localforage-memoryStorageDriver";
 import { setup } from "axios-cache-adapter";
 import { AxiosInstance } from "axios";
 
-// `async` wrapper to configure `localforage` and instantiate `axios` with `axios-cache-adapter`
 export default async function configure(): Promise<AxiosInstance> {
-  // Register the custom `memoryDriver` to `localforage`
-  await localforage.defineDriver(memoryDriver);
-
-  // Create `localforage` instance
-  const forageStore = localforage.createInstance({
-    // List of drivers used
-    driver: [localforage.INDEXEDDB, localforage.LOCALSTORAGE, memoryDriver._driver],
-    // Prefix all storage keys to prevent conflicts
-    name: "F95API-cache"
-  });
-
-  // Create `axios` instance with pre-configured `axios-cache-adapter` using a `localforage` store
   const agent = setup({
     // `axios` options
     baseURL: "http://f95zone.to",
 
     // `axios-cache-adapter` options
     cache: {
-      // Tell adapter to attempt using response headers
-      readHeaders: true,
-      store: forageStore, // Pass `localforage` store to `axios-cache-adapter`
+      limit: 1500,
+      // Keep cache for at most 15 minutes
+      maxAge: 15 * 60 * 1000,
       exclude: {
         // Only exclude PUT, PATCH and DELETE methods from cache
         methods: ["put", "patch", "delete"],
