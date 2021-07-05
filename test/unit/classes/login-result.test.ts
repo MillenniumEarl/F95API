e9b5c7895e402cc1474f239bf53a546202fcee66
MillenniumEarl <22562005+MillenniumEarl@users.noreply.github.com>
2021-07-05 14:26:59 +0200

    Removed double Describe method

--- test/unit/classes/login-result.test.ts
+++ test/unit/classes/login-result.test.ts
@@ -3,22 +3,23 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+// Public modules from npm
 import { expect } from "chai";
+
+// Modules from file
 import LoginResult from "../../../src/scripts/classes/login-result";
 
 export function suite(): void {
-  describe("Test class LoginResult", () => {
-    it("LoginResult - Test properties", () => {
-      // Local variables
-      const result = true;
-      const code = LoginResult.AUTH_SUCCESSFUL;
-      const message = "Test message";
+  it("LoginResult - Test properties", () => {
+    // Local variables
+    const result = true;
+    const code = LoginResult.AUTH_SUCCESSFUL;
+    const message = "Test message";
 
-      const loginResult = new LoginResult(result, code, message);
+    const loginResult = new LoginResult(result, code, message);
 
-      expect(loginResult.success).to.be.true;
-      expect(loginResult.code).to.be.equal(code);
-      expect(loginResult.message).to.be.equal(message);
-    });
+    expect(loginResult.success).to.be.true;
+    expect(loginResult.code).to.be.equal(code);
+    expect(loginResult.message).to.be.equal(message);
   });
 }
