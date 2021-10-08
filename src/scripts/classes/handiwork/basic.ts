17a2b77c9160a1f96c29cacd4c488eed8cfa8a34
Millennium Earl <22562005+MillenniumEarl@users.noreply.github.com>
2021-10-08 14:54:49 +0200

    Add cast method

--- src/scripts/classes/handiwork/basic.ts
+++ src/scripts/classes/handiwork/basic.ts
@@ -32,4 +32,12 @@ export default class Basic implements IBasic {
   public constructor(init?: Partial<Basic>) {
     Object.assign(this, init);
   }
+
+  public cast<T extends IBasic>(src: T): this {
+    Object.entries(src) // Obtains all the key from the source object
+      .filter(([key]) => key in this) // Get all the keys that are in both the objects
+      .map(([key, val]) => Object.assign(this[key], val)); // Save the key in this object
+
+    return this;
+  }
 }
