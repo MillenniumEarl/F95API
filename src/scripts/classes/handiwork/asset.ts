68dabca029855264253f53a93c2428a1371e1246
Millennium Earl <22562005+MillenniumEarl@users.noreply.github.com>
2021-10-08 20:22:52 +0200

    Partial should accept anything that implements Basic

--- src/scripts/classes/handiwork/asset.ts
+++ src/scripts/classes/handiwork/asset.ts
@@ -17,7 +17,7 @@ export default class Asset extends Basic implements IAsset {
   sku: string = "";
   //#endregion Properties
 
-  public constructor(init?: Partial<Asset>) {
+  public constructor(init?: Partial<Basic>) {
     super();
     Object.assign(this, init);
   }
