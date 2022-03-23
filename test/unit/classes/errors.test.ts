f45c47288a4fd52a201f82445d839cd1b26faeb5
MillenniumEarl <22562005+MillenniumEarl@users.noreply.github.com>
2022-03-23 16:22:02 +0100

    Update errors tests

--- test/unit/classes/errors.test.ts
+++ test/unit/classes/errors.test.ts
@@ -3,14 +3,19 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+// Public modules from npm
 import { expect } from "chai";
+
+// Modules from file
 import {
   BaseAPIError,
   GenericAxiosError,
   UnexpectedResponseContentType,
   INVALID_ERROR_ERROR,
   INVALID_ERROR_ID,
-  INVALID_ERROR_MESSAGE
+  INVALID_ERROR_MESSAGE,
+  AxiosRequestError,
+  AxiosResponseError
 } from "../../../src/scripts/classes/errors";
 
 export function suite(): void {
@@ -93,6 +98,46 @@ export function suite(): void {
         "Message value must be equal"
       );
       expect(genericAxiosError.error).to.be.equal(
+        ERROR,
+        "Error value must be equal"
+      );
+    });
+  });
+
+  describe("Test class AxiosRequestError", () => {
+    it("AxiosRequestError - Valid arguments", () => {
+      // Arguments
+      const args = { id: ID, message: MESSAGE, error: ERROR };
+
+      // Property call
+      const axiosRequestError = new AxiosRequestError(args);
+
+      expect(axiosRequestError.id).to.be.equal(ID, "ID value must be equal");
+      expect(axiosRequestError.message).to.be.equal(
+        MESSAGE,
+        "Message value must be equal"
+      );
+      expect(axiosRequestError.error).to.be.equal(
+        ERROR,
+        "Error value must be equal"
+      );
+    });
+  });
+
+  describe("Test class AxiosResponseError", () => {
+    it("AxiosResponseError - Valid arguments", () => {
+      // Arguments
+      const args = { id: ID, message: MESSAGE, error: ERROR };
+
+      // Property call
+      const axiosResponseError = new AxiosResponseError(args);
+
+      expect(axiosResponseError.id).to.be.equal(ID, "ID value must be equal");
+      expect(axiosResponseError.message).to.be.equal(
+        MESSAGE,
+        "Message value must be equal"
+      );
+      expect(axiosResponseError.error).to.be.equal(
         ERROR,
         "Error value must be equal"
       );
