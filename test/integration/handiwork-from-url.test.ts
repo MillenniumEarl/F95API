55881eab9f3a7c18c33a340109c1a655be11ded9
MillenniumEarl <22562005+MillenniumEarl@users.noreply.github.com>
2023-05-15 15:58:49 +0200

    Use async tests

--- test/integration/handiwork-from-url.test.ts
+++ test/integration/handiwork-from-url.test.ts
@@ -4,7 +4,8 @@
 // https://opensource.org/licenses/MIT
 
 // Public module from npm
-import { expect } from "chai";
+import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
 
 // Modules from file
 import { USER_NOT_LOGGED } from "../../src/scripts/classes/errors";
@@ -12,44 +13,47 @@ import HandiWork from "../../src/scripts/classes/handiwork/handiwork";
 import { getHandiworkFromURL } from "../../src/scripts/handiwork-from-url";
 import Shared from "../../src/scripts/shared";
 
+chai.use(chaiAsPromised);
+const { expect } = chai;
+
 export function suite(): void {
-  it("Get handiwork when not logged", function () {
+  it("Get handiwork when not logged", async function () {
     // Local variable
-    const URL = "www.google.com";
+    const URL = "https://www.google.com/";
 
     // Set the session as not logged
     Shared.setIsLogged(false);
 
-    expect(getHandiworkFromURL(URL, HandiWork)).to.be.rejectedWith(USER_NOT_LOGGED);
+    await expect(getHandiworkFromURL(URL, HandiWork)).to.be.rejectedWith(USER_NOT_LOGGED);
   });
 
-  it("Get handiwork from invalid URL", function () {
+  it("Get handiwork from invalid URL", async function () {
     // Local variable
     const URL = "www.thisurlhastoofewlevels";
 
     // Set the session as logged
     Shared.setIsLogged(true);
-
-    expect(getHandiworkFromURL(URL, HandiWork)).to.be.rejectedWith(/(is not a valid url)/);
+    
+    await expect(getHandiworkFromURL(URL, HandiWork)).to.be.rejectedWith(`'${URL}' is not a valid URL`);
   });
 
-  it("Get handiwork from not existing URL", function () {
+  it("Get handiwork from not existing URL", async function () {
     // Local variable
     const URL = "www.thisurldoesnotexists4688j765f8.not";
 
     // Set the session as logged
     Shared.setIsLogged(true);
-
-    expect(getHandiworkFromURL(URL, HandiWork)).to.be.rejectedWith(/(does not exists)/);
+    
+    await expect(getHandiworkFromURL(URL, HandiWork)).to.be.rejectedWith(`${URL} does not exists`);
   });
 
-  it("Get handiwork from non-F95Zone URL", function () {
+  it("Get handiwork from non-F95Zone URL", async function () {
     // Local variable
-    const URL = "www.google.com";
+    const URL = "https://www.google.com/";
 
     // Set the session as logged
     Shared.setIsLogged(true);
-
-    expect(getHandiworkFromURL(URL, HandiWork)).to.be.rejectedWith(/(is not a valid F95Zone URL)/);
+    
+    await expect(getHandiworkFromURL(URL, HandiWork)).to.be.rejectedWith(`${URL} is not a valid F95Zone URL`);
   });
 }
