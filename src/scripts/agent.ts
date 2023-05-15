fa0b0dad8f5b6548e86ea8295f21a972ff8ae744
MillenniumEarl <22562005+MillenniumEarl@users.noreply.github.com>
2023-05-15 12:09:45 +0200

    Axios adapters are not exported in version 1.0

--- src/scripts/agent.ts
+++ src/scripts/agent.ts
@@ -16,6 +16,14 @@ import axiosRetry from "axios-retry";
 import { wrapper as addCookieJarSupport } from "axios-cookiejar-support";
 
 /**
+ * Explicit the HTTP adapter otherwise on Electron the XHR adapter
+ * is used which is not supported by `axios-cookiejar-support`
+ * 
+ * From v1.0, the adapters are not exposed to API, waiting for patch...
+ */
+axios.defaults.adapter = "http";
+
+/**
  * User agent string used to describe this API.
  */
 const USER_AGENT = `Mozilla/5.0 (compatible; F95API/${version}; MillenniumEarl@f95zone; https://github.com/MillenniumEarl/F95API)`;
@@ -44,11 +52,6 @@ const commonConfig: AxiosRequestConfig = {
     return status < 500; // Resolve only if the status code is less than 500
   },
   timeout: 30000,
-  /**
-   * Explicit the HTTP adapter otherwise on Electron the XHR adapter
-   * is used which is not supported by `axios-cookiejar-support`
-   */
-  adapter: require("axios/lib/adapters/http")
 };
 
 /**
